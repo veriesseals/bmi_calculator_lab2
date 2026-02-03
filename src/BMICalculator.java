a7d43d90aa424e82c1d841374f926b2029589a8e
Veries Seals <veries@me.com>
2026-02-02 18:50:58 -0600

    Updated main and BMICalculator files

--- src/BMICalculator.java
+++ src/BMICalculator.java
@@ -1,6 +1,6 @@
 public class BMICalculator {
 
-    // declare the following four attributes/properties in the BMICalculator class to represent: weight in pounds, height in inches, weight in kilos, and height in meters
+    // Declare the following four attributes/properties in the BMICalculator class to represent: weight in pounds, height in inches, weight in kilos, and height in meters
     // -------------------------------------------------
     private double weightInPounds;
     private double heightInInches;
@@ -10,6 +10,11 @@ public class BMICalculator {
 
     // A no-argument constructor that initializes the four properties to some initial values
     // -------------------------------------------------
+
+    public double calculateBmiImperial(double weightInPounds, double heightInInches){
+        return (703 * weightInPounds) / (heightInInches * heightInInches);
+
+    }
 
 
 
