b0bada63c5c0c365c2060cda9ee674d94f3fdeed
Veries Seals <veries@me.com>
2026-02-02 18:49:43 -0600

    Added the calculateBmiImperial() method with its return statement

--- src/BMICalculator.java
+++ src/BMICalculator.java
@@ -1,3 +1,17 @@
 public class BMICalculator {
-    
+
+    // declare the following four attributes/properties in the BMICalculator class to represent: weight in pounds, height in inches, weight in kilos, and height in meters
+    // -------------------------------------------------
+    private double weightInPounds;
+    private double heightInInches;
+    private double weightInKilos;
+    private double heightInMeters;
+
+
+    // A no-argument constructor that initializes the four properties to some initial values
+    // -------------------------------------------------
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the BMI Calculator!");
+    }
     
 }
